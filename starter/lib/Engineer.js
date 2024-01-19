df2e0605f40691c358b5f425552b044c6e14a9da
cat coder <s.emama@hotmail.com>
2024-01-19 21:02:38 +0000

    Added logic to subclasses engineer and intern

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+// Links employee file to engineer file
+
+//Create subclass Engineer to inherit from Employee class
+class Engineer extends Employee {
+    constructor (name, id, email, github) {
+        super (name,id,email);
+        this.github = github;
+
+    }
+
+    getGithub () {
+        return this.github;
+    }
+
+
+    getRole () {
+        return 'Engineer';
+    }
+
+
+
+}
+
+
+module.exports = Engineer;
--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+// Links employee file to Intern file
+
+//Create subclass Intern to inherit from Employee class
+class Intern extends Employee {
+    constructor (name, id, email, school) {
+        super (name,id,email);
+        this.school = school;
+
+    }
+
+    getSchool () {
+        return this.school;
+    }
+
+
+    getRole () {
+        return 'Intern';
+    }
+
+
+
+}
+
+
+module.exports = Intern;
